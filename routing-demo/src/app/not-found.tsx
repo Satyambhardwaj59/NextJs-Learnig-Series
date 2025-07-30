f1be42e34e635fbb1349e21e97b32d0465586a2e
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-07-30 21:27:54 +0530

    explore 404 NOT found page and understand how will build as requriement

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+    return (
+        <div className="not-found">
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+        </div>
+    )
+};
+
+// Custimize error page for better user experience
---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewsId]/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import {usePathname} from "next/navigation";
+
+export default function NotFound() {
+    const pathname = usePathname();
+    const productId = pathname.split("/")[2];
+    const reviewsId = pathname.split("/")[4];
+    return <h1> Review {reviewsId} Not Found for product {productId}</h1>
+};
